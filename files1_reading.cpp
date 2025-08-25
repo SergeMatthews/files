601936a05683b39f4e9c830631feae8d844d724b
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-25 11:01:16 +0600

    Add files via upload

---
+++ files1_reading.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    string word;
+    ifstream fin("in.txt");
+    if (!fin.is_open())
+    {
+        cout << "Не удалось открыть файл." << endl;
+        return EXIT_FAILURE;
+    }
+    while (!fin.eof())
+    {
+        fin >> word;
+        cout << word << endl;
+    }
+    
+    fin.close();
+    return EXIT_SUCCESS;
+}
+
+
