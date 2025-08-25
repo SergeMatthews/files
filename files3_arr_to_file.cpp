5ea5f0a61b204746f31b4a09be69b2fa46381ab2
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-25 11:05:37 +0600

    Add files via upload

---
+++ files3_arr_to_file.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int arr_size = 0;
+    cout << "Введите размер массива: ";
+    cin >> arr_size;
+    int* arr = new int[arr_size] {};
+    for (int i = 0; i < arr_size; ++i)
+    {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+
+    ofstream fout("out.txt");
+    if (!fout.is_open())
+    {
+        cout << "Не удалось открыть файл" << endl;
+        return EXIT_FAILURE;
+    }
+    fout << arr_size << endl;
+    for (int i = arr_size - 1; i >= 0; --i)
+    {
+        fout << arr[i] << ' ';
+    }
+    
+    delete[] arr;
+    fout.close();
+    return EXIT_SUCCESS;
+}
+
